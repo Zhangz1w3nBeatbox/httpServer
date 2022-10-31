d0fe5039d8fe923ff69ccbdb22a352f95ea71d0a
zhangz1w3nbeatbox <zhangz1w3nbbx@163.com>
2022-10-31 17:18:06 +0800

    http1.0的单线程处理-短连接

--- src/main/java/com/zzw/httpServer/HttpServer.java
+++ src/main/java/com/zzw/httpServer/HttpServer.java
@@ -7,33 +7,60 @@ import java.util.Date;
 
 public class HttpServer {
 
-    public HttpServer(int port) throws IOException {
+    public HttpServer(int port) throws IOException, InterruptedException {
         if(port<1||port>65535) throw new MyRuntimeException("端口异常!");
 
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("服务器已经启动 正在监听"+port+"端口");
 
         while(true){
+
             Socket clientSocket = serverSocket.accept();
+
             System.out.println("接收到客户端请求:"+clientSocket.getPort());
 
             if(clientSocket!=null&&!clientSocket.isClosed()){
 
-                OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("HTTP/1.1 200 OK\r\n".getBytes());
-                outputStream.write("Server:HttpServer/1.0\r\n".getBytes());
-                outputStream.write(("Date:"+(new Date()).toString()+"\r\n").getBytes());
-                outputStream.write("Content-Type: text/html; charset=UTF-8\r\n".getBytes());
-                outputStream.write("\r\n".getBytes());
-                outputStream.write("<h1>Welcome Beatboxer!</h1>".getBytes());
-                outputStream.flush();
-                outputStream.close();
+                responseToClient(clientSocket);
+
             }
 
         }
+
+
     }
 
-    public static void main(String[] args) throws IOException {
+
+    //响应客户端的请求
+    public void responseToClient(Socket clientSocket) throws IOException, InterruptedException {
+
+        //模拟处理 耗时
+        Thread.sleep(3000);
+
+        OutputStream outputStream = clientSocket.getOutputStream();
+
+        //响应头信息
+
+        //状态行 版本号 状态码
+        outputStream.write("HTTP/1.0 200 OK\r\n".getBytes());
+
+        //首部行
+        outputStream.write("Server:HttpServer/1.0\r\n".getBytes());
+        outputStream.write(("Date:"+(new Date()).toString()+"\r\n").getBytes());
+        outputStream.write("Content-Type: text/html; charset=UTF-8\r\n".getBytes());
+
+
+        outputStream.write("\r\n".getBytes());
+
+        //响应-实体体
+        outputStream.write("<h1>Welcome Beatboxer!</h1>".getBytes());
+
+
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         HttpServer httpServer = new HttpServer(8383);
     }
 }
