134c23eca68149c9c6f90cce977a046e12921b06
zhangz1w3nbeatbox <zhangz1w3nbbx@163.com>
2022-10-31 20:32:25 +0800

    处理请求url-根据请求的url到服务器本地去寻找 找到后最后 发送到String中 最后返回

--- src/main/java/com/zzw/httpServer/HttpServer.java
+++ src/main/java/com/zzw/httpServer/HttpServer.java
@@ -1,9 +1,9 @@
 package com.zzw.httpServer;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.lang.String;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -71,27 +71,81 @@ public class HttpServer {
         outputStream.close();
     }
 
-    //响应客户端的请求
-    public void responseToClient(Socket clientSocket) throws IOException, InterruptedException {
-
-        //模拟处理 耗时
-        //Thread.sleep(3000);
-
+    //验证无效请求
+    private void validate(Socket clientSocket) throws IOException {
         //获取输出流
         OutputStream outputStream = clientSocket.getOutputStream();
         //获取输入流
         InputStream inputStream = clientSocket.getInputStream();
-
-
         if(inputStream.available()==0){
             toResponse(outputStream,200,"OK","text/html","<h1>OK</h1>");
             return;
         }
+    }
+
+    //响应客户端的请求
+    public void responseToClient(Socket clientSocket) throws IOException, InterruptedException {
+        //获取输出流
+        OutputStream outputStream = clientSocket.getOutputStream();
+
+        //获取输入流
+        InputStream inputStream = clientSocket.getInputStream();
 
         //处理无效请求
-        System.out.println("客户端的请求数据长度:"+inputStream.available());
+        validate(clientSocket);
 
-        toResponse(outputStream,200,"OK","text/html","<h1>主人欢迎回来!</h1>");
+        //处理无效请求
+        int availableDateLen = inputStream.available();
+        System.out.println("客户端的请求数据长度:"+availableDateLen);
+
+        //获取请求文档
+        byte[] readBuffer = new byte[availableDateLen];
+
+        inputStream.read(readBuffer);
+
+        String s = new String(readBuffer);
+
+        String[] lineArray = s.split("\r\n");
+        String requestLine = lineArray[0];
+
+        String[] requestLineArray = requestLine.split(" ");
+        String requestURL = requestLineArray[1];
+
+
+        //如果请求的路径是其他静态路径则进行处理
+        if(requestURL.equals("/favicon.ico")){
+            toResponse(outputStream,200,"OK","text/html","favicon.ico");
+            return;
+        }
+
+        //验证请求路径 如果是默认路径 也就是 '/'则去index页面 否则 去请求的对应页面
+        requestURL = requestURL.equals("/")? "index.html" :requestURL.substring(1);
+
+        //去服务器内部的resource文件夹 找 对应文件名的文件
+        URL resourcePath = getClass().getClassLoader().getResource(requestURL);
+
+        //如果文件不存在 返回404
+        if(resourcePath==null){
+            toResponse(outputStream,404,"Not Found","text/html","<h1>404 File Not Found!</h1>");
+            return;
+        }
+
+        //找得到就 去用bufferInputStream读出来
+        byte[] temp = new byte[2048];
+        int len = 0;//记录每次读取的有效字节个数
+
+        //然后把读到的内容放到responseDate中
+        String responseDate ="";
+
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(resourcePath.getPath()));){
+            while ((len = bis.read(temp))!=-1){
+                responseDate += new String(temp, 0, len);
+            }
+            bis.read(temp);
+        }
+
+        //最后返回
+        toResponse(outputStream,200,"OK","text/html",responseDate);
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
